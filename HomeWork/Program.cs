2b0f6fad5379078a876ef9f6d8ed30cdd35e69e9
Konstantin <love_road@mail.ru>
2022-08-09 12:08:20 +0300

    yslovie

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,10 +1,11 @@
 ﻿Console.Clear();
 
-string[] array = {"hello", "2", "world", ":-)"};
+string[] array = { "hello", "2", "world", ":-)" };
 string[] nearr = new string[array.Length];
 
 for (int i = 0; i < array.Length; i++)
 {
-    if(array[i].Length <= 3)
-       
+    if (array[i].Length <= 3)
+        newarr[i] = array[i];
+    Console.Write($"{newarr[i]} ");
 }
